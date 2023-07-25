601565eaf08dc048167932c4a58a7d1bde9669fc
aapsi <aapsi0001@gmail.com>
2023-07-25 13:45:55 +0530

    our calsses page

--- fitnessWebsite/src/scenes/ourClasses/index.tsx
+++ fitnessWebsite/src/scenes/ourClasses/index.tsx
@@ -1,6 +1,12 @@
 import { SelectedPage } from '@/shared/types'
 import { motion } from 'framer-motion';
 import Htext from '@/shared/Htext';
+import image1 from "@/assets/image1.png";
+import image2 from "@/assets/image2.png";
+import image3 from "@/assets/image3.png";
+import image4 from "@/assets/image4.png";
+import image5 from "@/assets/image5.png";
+import image6 from "@/assets/image6.png";
 
 
 type Props = {
